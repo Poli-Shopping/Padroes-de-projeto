3258116c3329002ee497c2f155f0e7953826b029
Vinícius Silva <vinicius27ferreirasilva@gmail.com>
2021-12-26 16:51:48 -0300

    terminado a criação de carro tipo regular

--- Template_method/regularCar.py
+++ Template_method/regularCar.py
@@ -15,23 +15,29 @@ class RegularCar(AbstractCar):
     def add_porta(self,qtd_porta,tipo_porta):
         self.porta = tipo_porta
         self.qtd_portas = qtd_porta
-        print(f'criando {qtd_porta} portas do tipo {tipo_porta}.')
+        print(f'criando {self.qtd_portas} portas do tipo {self.porta}.')
 
     def add_pneu(self,tipo_pneu):
         self.pneus = tipo_pneu
-    
+        print(f'adicionado pneu do tipo {self.pneus}')
+
     def add_parabrisa(self,tipo_parabrisa):
         self.parabrisa = tipo_parabrisa
-
+        print(f'adicionado parabrisa do tipo {self.parabrisa}')
   
     def add_motor(self, tipo_motor):
         self.motor = tipo_motor
-
+        print(f'adicionado motor do tipo {self.motor}')
    
     def abastecer(self):
-        pass
+        self.nivel_combustivel = 100
+        print(f'Nível atual do combutível {self.nivel_combustivel}%.')
 
- 
-    def add_cambio(self):
-        pass
+    def add_tipo_combustivel(self, tipo_combustivel):
+        self.tipo_combustivel = tipo_combustivel
+        print(f"Combustível do tipo {self.tipo_combustivel}")
+
+    def add_cambio(self,tipo_cambio):
+        self.cambio = tipo_cambio
+        print(f'adicionado cambio tipo {tipo_cambio}')
 
